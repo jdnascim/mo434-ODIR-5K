b893421643606f54c18dd6dc7cd78bd0637ce6ac
Jose Dori Nascimento <jnascimento@ssh1.recod>
2020-12-13 17:22:20 -0300

    finetuning updates

--- exps/1-baseline/finetuning_vgg16.py
+++ exps/1-baseline/finetuning_vgg16.py
@@ -5,10 +5,15 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 BATCH_SIZE = 64
-EPOCHS = 5
+EPOCHS = 3
 SEED = 13
+
+EXP_NAME = "ft_vgg16_fc2"
+
+tf.random.set_seed(SEED)
 
 vgg_conv = VGG16(weights='imagenet')
 
@@ -21,7 +26,7 @@ out = Dense(8, activation='softmax', name='predictions')(out)
 model = Model(vgg_conv.input, out)
 
 # We compile the model
-model.compile(optimizer=Adam(lr=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=Adam(lr=0.001), loss='categorical_crossentropy', metrics=['AUC'])
 
 datagen = ImageDataGenerator(validation_split=0.2)
 
@@ -40,11 +45,15 @@ history = model.fit(
         validation_steps=val_gen.n // BATCH_SIZE)
 
 loss_values = history.history['loss']
+auc_values = history.history['val_auc']
 epochs = range(1, len(loss_values)+1)
 
 plt.plot(epochs, loss_values, label='Training Loss')
+plt.plot(epochs, auc_values, label='Training AUC')
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.legend()
 
-plt.savefig("./test.png")
+plt.savefig(EXP_NAME + ".png")
+
+model.save_weights(EXP_NAME + ".h5")
